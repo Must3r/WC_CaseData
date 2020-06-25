ca21efdf2b8365a884e5751dc03ea3eb08de781a
Stanislav Kushnir <sk@workconsult.ua>
2020-06-25 17:21:50 +0300

    refs:#8106 minor changes

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,9 @@
             class="position-relative d-flex justify-content-between align-items-center p-0"
             style="min-height: 32px"
           >
-            <h2 style="z-index:1" class="text-left h5 mb-0 pl-3">{{ section.title }}</h2>
+            <h2 style="z-index:1" class="text-left h4 mb-0 pl-3">
+              <small>{{ section.title }}</small>
+            </h2>
             <b-progress
               v-if="progress[section.name] && section.name !== '3'"
               :value="progress[section.name].count"
@@ -262,7 +264,8 @@ export default {
     fieldsModels: {},
     options: [
       { text: "Yes", value: true },
-      { text: "No", value: false }
+      { text: "No", value: false },
+      { text: "Null", value: null }
     ],
     apiOptions: {},
     urlParams: {},
@@ -276,10 +279,10 @@ export default {
   }),
   methods: {
     init () {
-      // const url = (window.location != window.parent.location)
-      //   ? document.referrer
-      //   : document.location.href
-      const url = 'https://job-server.net/js/case_data/?sid=wconen&applicant_id=1313'
+      const url = (window.location != window.parent.location)
+        ? document.referrer
+        : document.location.href
+      // const url = 'https://job-server.net/js/case_data/?sid=wconen&applicant_id=1313131'
       this.getParams(url)
       this.getFields(`/casedata?a=init&sid=wconen&applicant_id=${this.urlParams.applicant_id}`)
     },
